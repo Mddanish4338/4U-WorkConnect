462f979095d6a36076951d2c151ae4bd4bf94f1a
DANISH <124383478+Mddanish4338@users.noreply.github.com>
2025-05-06 02:52:59 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,13 +4,7 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAh3kwE-BWKbCWekUrlzpxhmAOL9pcGCDU",
-    authDomain: "u-data-3cbc5.firebaseapp.com",
-    projectId: "u-data-3cbc5",
-    storageBucket: "u-data-3cbc5.firebasestorage.app",
-    messagingSenderId: "1349410075",
-    appId: "1:1349410075:web:6b89b407586147f4b7c7be",
-    measurementId: "G-RBY8M25GXF"
+
   };
 
 const app = initializeApp(firebaseConfig);
